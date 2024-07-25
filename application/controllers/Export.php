c478da90673df0c744e57b00fcf310bf1afdbcb0
didik setya <didiksetyaone0@yahoo.com>
2024-07-25 20:47:18 +0700

    function export

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -1,9 +1,21 @@
 <?php
 defined('BASEPATH') or exit('No direct script accesss allowed');
 date_default_timezone_set('Asia/Jakarta');
+
+require 'assets/phpspreadsheet/vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class Export extends CI_Controller
 {
-    public function export()
+    public function export_data()
     {
+        $spreadsheet = new Spreadsheet();
+        $activeWorksheet = $spreadsheet->getActiveSheet();
+        $activeWorksheet->setCellValue('A1', 'Hello World !');
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('hello world.xlsx');
     }
 }
