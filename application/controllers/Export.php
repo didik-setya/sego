9b35d64bd1220e96deb08a5ddb71ae23c663bb1a
didik setya <didiksetyaone0@yahoo.com>
2024-07-25 20:41:10 +0700

    add extension phpspreadsheet

---
+++ application/controllers/Export.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') or exit('No direct script accesss allowed');
+date_default_timezone_set('Asia/Jakarta');
+class Export extends CI_Controller
+{
+    public function export()
+    {
+    }
+}
